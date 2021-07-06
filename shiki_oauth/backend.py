813b1c5f1134089585f45b9b7d9e245b8aec7d36
Daniil <dadyarri@gmail.com>
2021-07-06 18:52:18 +0300

    feat(oauth): add shikimori oauth backend

--- shiki_oauth/backend.py
+++ shiki_oauth/backend.py
@@ -1,5 +1,4 @@
 import os
-from urllib import urlencode
 
 from social_core.backends.oauth import BaseOAuth2
 
@@ -11,8 +10,7 @@ class ShikimoriOAuth2(BaseOAuth2):
     AUTHORIZATION_URL = os.getenv("SHIKI_AUTH_URL")
     ACCESS_TOKEN_URL = os.getenv("SHIKI_ACCESS_TOKEN_ENDPOINT")
     ACCESS_TOKEN_METHOD = os.getenv("SHIKI_ACCESS_TOKEN_METHOD")
-    SCOPE_SEPARATOR = ","
-    EXTRA_DATA = [("id", "id"), ("expires", "expires")]
+    SCOPE_SEPARATOR = "+"
 
     def get_user_details(self, response):
         """Return user details from Shikimori account.
@@ -29,16 +27,15 @@ class ShikimoriOAuth2(BaseOAuth2):
             "first_name": response.get("name"),
         }
 
-    def user_data(self, access_token, *args, **kwargs):
+    def user_data(self, *args, **kwargs):
         """Loads user data from service.
         
         Args:
-            access_token: API access token
             args: additional params
             kwargs: additional params
 
         Returns:
             dict: User's data
         """
-        url = "https://api.github.com/user?" + urlencode({"access_token": access_token})
+        url = "https://shikimori.one/api/users/whoami"
         return self.get_json(url)
