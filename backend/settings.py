b993c317d1489f664fae376392f1ffa769e9ebb6
Daniil <dadyarri@gmail.com>
2021-07-07 12:16:33 +0300

    fix(django): update l10n settings

--- backend/settings.py
+++ backend/settings.py
@@ -110,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
-USE_I18N = True
+USE_I18N = False
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
